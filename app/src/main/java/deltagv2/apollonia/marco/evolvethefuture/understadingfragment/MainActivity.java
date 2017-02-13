4e7a373f15d2d3fd6bf881bc5cda844439888ad2
marco <apolloniamarco93@gmail.com>
2017-02-13 17:26:48 +0100

    Modified Activity Add new File Test Git

--- app/src/main/java/deltagv2/apollonia/marco/evolvethefuture/understadingfragment/MainActivity.java
+++ app/src/main/java/deltagv2/apollonia/marco/evolvethefuture/understadingfragment/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void azione(View v){
+
+        Toast t= Toast.makeText(this,"Hello", Toast.LENGTH_LONG);
+        t.show();
+
+
         ArticleFragment newFragment = new ArticleFragment();
         Bundle args = new Bundle();
         newFragment.setArguments(args);
---
+++ app/src/main/java/deltagv2/apollonia/marco/evolvethefuture/understadingfragment/TestGit.java
@@ -0,0 +1,7 @@
+package deltagv2.apollonia.marco.evolvethefuture.understadingfragment;
+
+/**
+ * Created by marco on 13/02/2017.
+ */
+public class TestGit {
+}
